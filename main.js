d6f907b25a997e45087c9a59ae8c4eec427e3433
lehn <laurence.for5@gmail.com>
2024-02-15 00:58:20 +0900

    update commit

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ let taskList = [];
 addButton.addEventListener('click', addTask);
 
 function addTask() {
-  let taskContent = taskInput.value;
-  taskList.push(taskContent);
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
   console.log(taskList);
   render();
 }
@@ -14,13 +18,48 @@ function addTask() {
 function render() {
   let resultHTML = '';
   for (let i = 0; i < taskList.length; i++) {
-    resultHTML += `<div class="task">
-    <div>${taskList[i]}</div>
+    if (taskList[i].isComplete == true) {
+      resultHTML += `<div class="task">
+      <div class="task-done">${taskList[i].taskContent}</div>
+      <div>
+        <i class="fa-solid fa-rotate-left" onclick="toggleComplete('${taskList[i].id}')"></i>
+        <i class="fa-solid fa-trash" onclick="deleteTask('${taskList[i].id}')"></i>
+      </div>
+    </div>`;
+    } else {
+      resultHTML += `<div class="task">
+    <div class="task-progress">${taskList[i].taskContent}</div>
     <div>
-      <button>Check</button>
-      <button>Delete</button>
+      <i class="fa-solid fa-check" onclick="toggleComplete('${taskList[i].id}')"></i>
+      <i class="fa-solid fa-trash" onclick="deleteTask('${taskList[i].id}')"></i>
     </div>
   </div>`;
+    }
   }
   document.getElementById('task-board').innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+  console.log(taskList);
+}
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList = taskList.filter((id) => id !== taskList[i]);
+      break;
+    }
+  }
+  render();
+  console.log(taskList);
+}
+
+function randomIDGenerate() {
+  return '_' + Math.random().toString(36).substring(2, 11);
 }
